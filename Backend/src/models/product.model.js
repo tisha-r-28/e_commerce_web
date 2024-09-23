6c31e93da7504e2497b1398d5231ecf351f1536f
tisha-r-28 <tisha298.rejoice@gmail.com>
2024-09-23 14:03:33 +0530

    Product model created.

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["Men", "Women"],
+        required: true
+    },
+    subCategory: {
+        type: String,
+        required: true
+    },
+    availability: {  
+        inStock: {  
+            type: Boolean,  
+            default: true  
+        },  
+        stockQuantity: {  
+            type: Number,  
+            required: true,  
+            default: 0  
+        }  
+    },
+    salePrice: {  
+        type: Number,  
+        default: null  
+    },  
+    isOnSale: {  
+        type: Boolean,  
+        default: false  
+    },
+    colors: {
+        type: String,
+    },
+    images: [{  
+        url: {  
+            type: String,  
+            required: true  
+        },  
+        altText: {  
+            type: String,  
+            required: true  
+        }  
+    }],
+    ratings: {  
+        averageRating: {  
+            type: Number,  
+            default: 0.0  
+        },  
+        totalReviews: {  
+            type: Number,  
+            default: 0  
+        }  
+    }, 
+    shippingCost: {  
+        type: Number,  
+        default: 0  
+    },  
+    returnsPolicy: {  
+        type: String,  
+        default: "05 days return policy."  
+    }  
+});
+
+const User = mongoose.model('products', productSchema);
+module.exports = User;
