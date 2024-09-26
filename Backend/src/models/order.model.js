2783ad2ad85529f03f01073d205656f4b1bb858e
tisha-r-28 <tisha298.rejoice@gmail.com>
2024-09-26 09:28:49 +0530

    Added fullName field at shippingAddress schema on order model

--- Backend/src/models/order.model.js
+++ Backend/src/models/order.model.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const shippingAddressSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
     address: { 
         type: String, 
         required: true 
